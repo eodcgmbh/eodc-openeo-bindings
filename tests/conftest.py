887699ca9ed071f4d21510dcfe3a96f94a907143
Luca Foresta <luca.foresta@eodc.eu>
2019-09-19 11:17:22 +0200

    first commit, repo structure

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Dummy conftest.py for eodc_openeo_bindings.
+
+    If you don't know what this is for, just leave it empty.
+    Read more about conftest.py under:
+    https://pytest.org/latest/plugins.html
+"""
+
+# import pytest
