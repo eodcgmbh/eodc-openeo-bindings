e36b718380868f540bcf27bd2374e3220649b895
Luca Foresta <luca.foresta@eodc.eu>
2019-09-24 11:08:23 +0200

    added/updated tests

--- tests/test_write_airflow_dag.py
+++ tests/test_write_airflow_dag.py
@@ -1,12 +1,20 @@
+"""
+
+"""
+
+
 import os
-from eodc_openeo_bindings.write_jupyter_nb import write_jupyter_nb
+from eodc_openeo_bindings.write_airflow_dag import write_airflow_dag
 
 
-os.environ['CSW_SERVER'] = 'https://csw.eodc.eu'
 FOLDER = os.path.join(os.getcwd(), 'tests')
+os.environ['CSW_SERVER'] = 'https://csw.eodc.eu'
+os.environ['AIRFLOW_DAGS'] = FOLDER
 
 evi_file = os.path.join(FOLDER, 'process_graphs/evi.json')
-out_filepath = os.path.join(FOLDER, "openeo_job.ipynb")
 job_data = os.path.join(FOLDER, 'openeo_job')
 
-write_jupyter_nb(evi_file, job_data, nb_filepath=out_filepath)
+job_id = "jb-12345"
+user_name = "jdoe_67890"
+
+write_airflow_dag(job_id, user_name, process_graph_json=evi_file, job_data=job_data)
