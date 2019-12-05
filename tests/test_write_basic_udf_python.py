218a43bc3abe649b6dd06402b4106cd0d0882037
Luca Foresta <luca.foresta@eodc.eu>
2019-12-05 12:13:05 +0100

    update tests

--- tests/test_write_basic_udf_python.py
+++ tests/test_write_basic_udf_python.py
@@ -10,9 +10,9 @@ from eodc_openeo_bindings.write_basic_job import write_basic_job
 os.environ['CSW_SERVER'] = 'http://127.0.0.1:8000/'
 FOLDER = os.path.join(os.getcwd(), 'tests')
 
-evi_file = os.path.join(FOLDER, 'process_graphs/udf_r.json')
-job_data = os.path.join(FOLDER, 'output_udf_r')
-out_filepath = os.path.join(FOLDER, "basic_udf_r.py")
+evi_file = os.path.join(FOLDER, 'process_graphs/udf_python.json')
+job_data = os.path.join(FOLDER, 'output_udf_python')
+out_filepath = os.path.join(FOLDER, "basic_udf_python.py")
 
 output_format, output_folder = write_basic_job(evi_file, job_data, python_filepath=out_filepath)
 assert output_format == 'Gtiff'
