3729f1564ea3f3d165fd897460c195a9f9141a4c
sherrmann <sophie.herrmann@eodc.eu>
2020-05-13 12:43:17 +0200

    Remove JupyterNotebookWriter
    not required anymore
    
    Signed-off-by: sherrmann <sophie.herrmann@eodc.eu>

--- src/eodc_openeo_bindings/job_writer/file_handler.py
+++ src/eodc_openeo_bindings/job_writer/file_handler.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-import nbformat as nbf
 
 
 class FileHandler(ABC):
@@ -33,41 +32,3 @@ class BasicFileHandler(FileHandler):
     def append(self, content):
         self.file.write(content)
         return self.file
-
-
-class JupyterNotebookFileHandler(FileHandler):
-
-    def open(self):
-        job = nbf.v4.new_notebook()  # Instantiate jupyter notebook
-        job['metadata'] = self.get_nb_metadata()  # Set kernel
-        return job
-
-    def close(self):
-        # Write data to file!
-        nbf.write(self.file, self.filepath)
-
-    def append(self, content: str):
-        self.file['cells'].append(nbf.v4.new_code_cell(content))
-        return self.file
-
-    def get_nb_metadata(self):
-        return {
-            "kernelspec": {
-                "display_name": "Python [conda env:eoDataReaders]",
-                "language": "python",
-                "name": "conda-env-eoDataReaders-py"
-            },
-            "language_info": {
-                "codemirror_mode": {
-                    "name": "ipython",
-                    "version": 3
-                },
-                "file_extension": ".py",
-                "mimetype": "text/x-python",
-                "name": "python",
-                "nbconvert_exporter": "python",
-                "pygments_lexer": "ipython3",
-                "version": "3.7.3"
-            }
-        }
-
