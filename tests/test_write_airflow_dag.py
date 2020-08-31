eebb995b99fa2af98d2f82c89abc033dbaa99cca
Luca Foresta <luca.foresta@eodc.eu>
2020-08-31 21:00:20 +0200

    add/modify ref jobs

--- tests/test_write_airflow_dag.py
+++ tests/test_write_airflow_dag.py
@@ -64,13 +64,15 @@ def test_airflow_dag_parallel(csw_server, test_folder, evi_file, setup_airflow_d
 
     job_data = os.path.join(test_folder, 'openeo_job')
 
-    job_id = "jb-12345"
+    job_id = "jb-12347"
     user_name = "jdoe_67890"
     writer = AirflowDagWriter()
     writer.write_and_move_job(job_id=job_id, user_name=user_name, process_graph_json=evi_file, job_data=job_data,
-                              process_defs=backend_processes, vrt_only=True)
+                              process_defs=backend_processes, vrt_only=True, add_delete_sensor=True, add_parallel_sensor=True)
 
     # Simulate that job has ran (place vrt files in node folders)
+    if os.path.isdir(os.path.join(test_folder, 'openeo_job')):
+        rmtree(os.path.join(test_folder, 'openeo_job'))
     copytree(airflow_job_folder, job_data)
     # Recreate (parallelised DAG)
     writer = AirflowDagWriter()
@@ -82,15 +84,22 @@ def test_airflow_dag_parallel(csw_server, test_folder, evi_file, setup_airflow_d
     
     rmtree(os.path.join(test_folder, 'openeo_job'))
     
-    out_filepath = os.path.join(os.environ['AIRFLOW_DAGS'], 'dag_' + job_id + '_parallelize.py')
-
+    # Check DAG before parallelisation
+    out_filepath = os.path.join(os.environ['AIRFLOW_DAGS'], 'dag_' + job_id + '.py')
     with open(out_filepath) as outfile:
         out_content = outfile.read()
-
     ref_filepath = out_filepath.replace('.py', '_ref.py').replace(os.environ['AIRFLOW_DAGS'], os.environ['REF_JOBS'])
     with open(ref_filepath) as outfile:
         ref_content = outfile.read()
-
+    assert out_content == ref_content
+    
+    # Check DAG after parallelisation
+    out_filepath = os.path.join(os.environ['AIRFLOW_DAGS'], 'dag_' + job_id + '_parallelize.py')
+    with open(out_filepath) as outfile:
+        out_content = outfile.read()
+    ref_filepath = out_filepath.replace('.py', '_ref.py').replace(os.environ['AIRFLOW_DAGS'], os.environ['REF_JOBS'])
+    with open(ref_filepath) as outfile:
+        ref_content = outfile.read()
     assert out_content == ref_content
 
 
