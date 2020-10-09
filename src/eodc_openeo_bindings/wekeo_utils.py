a437ef860887e0fde1fe29aabef7a557ac59e756
Luca Foresta <luca.foresta@eodc.eu>
2020-10-09 09:49:09 +0200

    pass correct input to functions

--- src/eodc_openeo_bindings/wekeo_utils.py
+++ src/eodc_openeo_bindings/wekeo_utils.py
@@ -11,8 +11,7 @@ def request_wekeo_token(wekeo_url, username, password):
     if not response.ok:
         raise Exception(response.text)
     return {
-            "Authorization": "Bearer " + "5946b682-c8d9-3635-9e91-43a4beb98000",
-            #"Authorization": "Bearer " + response.json()["access_token"],
+            "Authorization": "Bearer " + response.json()["access_token"],
             "Accept": "application/json"
         }
 
@@ -61,7 +60,7 @@ def wrap_request(func):
             kwargs['headers'] = request_wekeo_token()
             response = func(*args, **kwargs)
         if not response.ok:
-            kwargs['headers'] = request_wekeo_token()
+            raise Exception(response.text)
 
         response = func(*args, **kwargs)
 
@@ -69,7 +68,8 @@ def wrap_request(func):
     return wrapper_func
 
 
-def download_wekeo_data(wekeo_job_id, item_url, output_filepath):
+def download_wekeo_data(wekeo_url, username, password,
+                        wekeo_job_id, item_url, output_filepath):
 
     # Decorate functions (because of WEkEO tokens lasting 1hour)
     request_wekeo_dataorder = wrap_request(request_wekeo_dataorder)
@@ -83,17 +83,18 @@ def download_wekeo_data(wekeo_job_id, item_url, output_filepath):
     f_dir = os.path.dirname(output_filepath)
 
     # Get token
-    headers = request_wekeo_token()
+    headers = request_wekeo_token(wekeo_url, username, password)
 
     # Create a WEkEO dataorder
-    order_id_url = request_wekeo_dataorder(wekeo_job_id, item_url)
+    order_id_url = request_wekeo_dataorder(wekeo_url, wekeo_job_id,
+                                           item_url, headers)
 
     # Check dataorder status
-    _ = request_check_order_status(order_id_url)
+    _ = request_check_order_status(order_id_url, headers)
 
     # Download file
     if not os.path.isfile(output_filepath_nc):
-        response = request_download(order_id_url)
+        response = request_download(order_id_url, headers)
 
         with open(output_filepath_zip, "wb") as f:
             for chunk in response.iter_content(chunk_size=1024):
