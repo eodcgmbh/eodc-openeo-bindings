552d637f7fb8d56e145379ac15c999d5831a3e1d
Luca Foresta <luca.foresta@eodc.eu>
2020-10-27 14:13:57 +0100

    add mapin

--- src/eodc_openeo_bindings/map_comparison_processes.py
+++ src/eodc_openeo_bindings/map_comparison_processes.py
@@ -13,3 +13,48 @@ def map_lt(process):
     param_dict = {'y': 'float'}
 
     return map_default(process, 'lt', 'apply', param_dict)
+
+
+def map_lte(process):
+    """
+
+    """
+
+    param_dict = {'y': 'float'}
+
+    return map_default(process, 'lte', 'apply', param_dict)
+
+
+def map_gt(process):
+    """
+
+    """
+
+    param_dict = {'y': 'float'}
+
+    return map_default(process, 'gt', 'apply', param_dict)
+
+
+def map_gte(process):
+    """
+
+    """
+
+    param_dict = {'y': 'float'}
+
+    return map_default(process, 'gte', 'apply', param_dict)
+
+
+def map_eq(process):
+    """
+
+    """
+
+    param_dict = {'y': 'numpy.array'}
+    # NOTE: how to map type dynamically to support strings?
+    if 'delta' in process['arguments']:
+        param_dict['delta'] = 'int'
+    if 'case_sensitive' in process['arguments']:
+        param_dict['case_sensitive'] = 'bool'
+
+    return map_default(process, 'eq', 'apply', param_dict)
--- tests/test_map_comparison_processes.py
+++ tests/test_map_comparison_processes.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import numpy as np
 
 from eodc_openeo_bindings.map_comparison_processes import *
@@ -10,12 +11,91 @@ def test_map_lt(eodatareaders_file, eodatareaders_params):
         "process_id": "lt",
         "arguments": {
             "x": {"from_node": "node_1"},
-            "y": 1000,
+            "y": 567,
         }
     }
 
     params = map_lt(process)
     eodatareaders_params.extend(params)
     eo = EODataProcessor([eodatareaders_file], eodatareaders_params)
-    assert eo.get_data(band='B04')[0,0] == 576 and eo.get_data(band='lt')[0,0] == 1
+    assert eo.get_data(band='B04')[0,0] == 576 and eo.get_data(band='lt')[0,0] == 0
     assert eo.get_data(band='B04')[-1,-1] == 2567 and eo.get_data(band='lt')[-1,-1] == 0
+
+
+def test_map_lte(eodatareaders_file, eodatareaders_params):
+
+    process = {
+        "process_id": "lte",
+        "arguments": {
+            "x": {"from_node": "node_1"},
+            "y": 576,
+        }
+    }
+
+    params = map_lte(process)
+    eodatareaders_params.extend(params)
+    eo = EODataProcessor([eodatareaders_file], eodatareaders_params)
+    assert eo.get_data(band='B04')[0,0] == 576 and eo.get_data(band='lte')[0,0] == 1
+    assert eo.get_data(band='B04')[-1,-1] == 2567 and eo.get_data(band='lte')[-1,-1] == 0
+
+
+def test_map_gt(eodatareaders_file, eodatareaders_params):
+
+    process = {
+        "process_id": "gt",
+        "arguments": {
+            "x": {"from_node": "node_1"},
+            "y": 576,
+        }
+    }
+
+    params = map_gt(process)
+    eodatareaders_params.extend(params)
+    eo = EODataProcessor([eodatareaders_file], eodatareaders_params)
+    assert eo.get_data(band='B04')[0,0] == 576 and eo.get_data(band='gt')[0,0] == 0
+    assert eo.get_data(band='B04')[-1,-1] == 2567 and eo.get_data(band='gt')[-1,-1] == 1
+
+
+def test_map_gte(eodatareaders_file, eodatareaders_params):
+
+    process = {
+        "process_id": "gte",
+        "arguments": {
+            "x": {"from_node": "node_1"},
+            "y": 576,
+        }
+    }
+
+    params = map_gte(process)
+    eodatareaders_params.extend(params)
+    eo = EODataProcessor([eodatareaders_file], eodatareaders_params)
+    assert eo.get_data(band='B04')[0,0] == 576 and eo.get_data(band='gte')[0,0] == 1
+    assert eo.get_data(band='B04')[-1,-1] == 2567 and eo.get_data(band='gte')[-1,-1] == 1
+
+
+def test_map_eq(eodatareaders_file, eodatareaders_params):
+
+    process = [
+        {
+            "process_id": "eq",
+            "arguments": {
+                "x": {"from_node": "node_1"},
+                "y": 576,
+            }
+        },
+        {
+            "process_id": "eq",
+            "arguments": {
+                "x": {"from_node": "node_1"},
+                "y": 577,
+                "delta": 1
+            }
+        }
+    ]
+
+    for proc in process:
+        eodatareaders_params2 = deepcopy(eodatareaders_params)
+        params = map_eq(proc)
+        eodatareaders_params2.extend(params)
+        eo = EODataProcessor([eodatareaders_file], eodatareaders_params2)
+        assert eo.get_data(band='B04')[0,0] == 576 and eo.get_data(band='eq')[0,0] == 1
